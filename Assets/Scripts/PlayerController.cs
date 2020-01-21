04a5bdb22502d50b685a98f8f6823799ab1ae5c1
Dmitry Orlov <perfectdaemon@gmail.com>
2020-01-22 03:19:28 +0700

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public const float ConstantMoveVelocity = 3;
+
+    public const float JumpVelocity = 5;
+
+    public Transform GroundChecker;
+
+    public float GroundRadius;
+
+    public LayerMask GroundLayer;
+
+    private Rigidbody2D rb;
+
+    private bool isOnGround;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(ConstantMoveVelocity, rb.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(GroundChecker.position, GroundRadius, GroundLayer);
+
+        if (isOnGround && Input.GetMouseButtonDown(0))
+            rb.velocity += new Vector2(0, JumpVelocity);
+    }
+}
---
+++ Assets/Scripts/TraceController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class TraceController : MonoBehaviour
+{
+    public bool LockY;
+
+    public Transform FollowIt;
+
+    private Transform selfTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selfTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selfTransform.position = new Vector3(
+            FollowIt.position.x,
+            LockY
+                ? selfTransform.position.y
+                : FollowIt.position.y,
+            selfTransform.position.z
+        );
+    }
+}
